cf1d1786c5657dd2a042d808662bdc2196c30fc4
YUFEI WANG <2837919089@qq.com>
2026-06-01 02:44:18 +0800

    Update upsampling.m

--- upsampling.m
+++ upsampling.m
@@ -1,27 +1,27 @@
-%放大 2 倍
+%Magnify twice
 img = im2double(imread('cameraman.tif'));
 [H, W] = size(img);
-scale = 2; % 放大倍数
+scale = 2; 
 
-% 1.FFT 
+% 1. FFT
 F = fftshift(fft2(img));
 
+% 2. Calculate the new dimensions
 newH = H * scale;
 newW = W * scale;
 F_new = zeros(newH, newW);
 
-% 3. insertzero
+% 3.Frequency domain zero padding
 r_start = floor((newH - H) / 2) + 1;
 c_start = floor((newW - W) / 2) + 1;
 F_new(r_start : r_start + H - 1, c_start : c_start + W - 1) = F;
-
-% 4. give back energy
+% 4.Energy compensation
 F_new = F_new * (scale^2);
 
 % 5.IFFT
 img_up = real(ifft2(ifftshift(F_new)));
 
-% result
+% Results
 figure('Name', 'FFT Upsampling');
 subplot(1,2,1); imshow(img); title(['initial ', num2str(H), 'x', num2str(W)]);
 subplot(1,2,2); imshow(img_up); title(['after upsampling ', num2str(newH), 'x', num2str(newW)]);
