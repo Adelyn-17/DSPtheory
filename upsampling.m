a8aeb0f3ef9328f81ca77e6d95989d8fdb6ffee5
YUFEI WANG <2837919089@qq.com>
2026-05-31 17:33:16 +0800

    Refactor FFT-based image upsampling code

---
+++ upsampling.m
@@ -0,0 +1,27 @@
+%放大 2 倍
+img = im2double(imread('cameraman.tif'));
+[H, W] = size(img);
+scale = 2; % 放大倍数
+
+% 1.FFT 
+F = fftshift(fft2(img));
+
+newH = H * scale;
+newW = W * scale;
+F_new = zeros(newH, newW);
+
+% 3. insertzero
+r_start = floor((newH - H) / 2) + 1;
+c_start = floor((newW - W) / 2) + 1;
+F_new(r_start : r_start + H - 1, c_start : c_start + W - 1) = F;
+
+% 4. give back energy
+F_new = F_new * (scale^2);
+
+% 5.IFFT
+img_up = real(ifft2(ifftshift(F_new)));
+
+% result
+figure('Name', 'FFT Upsampling');
+subplot(1,2,1); imshow(img); title(['initial ', num2str(H), 'x', num2str(W)]);
+subplot(1,2,2); imshow(img_up); title(['after upsampling ', num2str(newH), 'x', num2str(newW)]);
