7458cae55cae7ceb3979f268b131929a7e128c6f
YUFEI WANG <2837919089@qq.com>
2026-06-01 02:46:39 +0800

    Update dowmsampling.m

--- dowmsampling.m
+++ dowmsampling.m
@@ -1,27 +1,27 @@
-%缩小为 0.5 倍
+%% Reduced to 0.5 times
 img = im2double(imread('cameraman.tif'));
 [H, W] = size(img);
 scale = 0.5;
 
-% 1.  FFT 
+% 1. FFT
 F = fftshift(fft2(img));
 
-% 2. 
+% 2. Calculate new dimensions
 newH = round(H * scale);
 newW = round(W * scale);
 
-% 3. truncate
+% 3. Frequency domain truncation
 r_start = floor((H - newH) / 2) + 1;
 c_start = floor((W - newW) / 2) + 1;
 F_new = F(r_start : r_start + newH - 1, c_start : c_start + newW - 1);
 
-% 4. give back energy
+% 4.Energy compensation
 F_new = F_new * (scale^2);
 
-% 5. ITTT
+% 5. IFFT
 img_down = real(ifft2(ifftshift(F_new)));
 
-% result
+% results
 figure('Name', 'FFT Downsampling');
 subplot(1,2,1); imshow(img); title(['initial ', num2str(H), 'x', num2str(W)]);
 subplot(1,2,2); imshow(img_down); title(['after downsampling ', num2str(newH), 'x', num2str(newW)]);
